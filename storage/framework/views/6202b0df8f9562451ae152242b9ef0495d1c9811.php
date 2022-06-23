4c00b0c070d7c160adc45c082ed016d8d9dae1b9
omar1890 <omar_wael1890@yahoo.com>
2022-06-23 09:29:55 +0200

    cache views and add .gitignore

--- storage/framework/views/6202b0df8f9562451ae152242b9ef0495d1c9811.php
+++
@@ -1,7 +0,0 @@
-
-<div class="col-lg-12">
-    <div class="section-title text-center pb-50 pt-50">
-        <span class="title-tag"><?php echo e($module['title']); ?></span>
-       
-    </div>
-</div><?php /**PATH /home/customer/www/new.vadecom.net/public_html/core/resources/views/admin/modules/themes_for_modules/title_paragraph.blade.php ENDPATH**/ ?>
